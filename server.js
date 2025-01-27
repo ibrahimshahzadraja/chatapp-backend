04907643ba386058203afc9908e55b59ced33a76
ibrahim <ibrahimdvp1@gmail.com>
2025-01-27 19:08:19 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+import express from "express";
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("joinRoom", (chatname) => {
+    socket.join(chatname);
+    console.log(`User joined room: ${chatname}`);
+  });
+
+  socket.on("leaveRoom", (chatname) => {
+    socket.leave(chatname);
+    console.log(`User left room: ${chatname}`);
+  });
+
+  socket.on("sendMessage", ({ chatname, username, message }) => {
+    console.log(`Message sent to room ${chatname}: ${message}`);
+    socket.to(chatname).emit("message", username, message);
+  });
+
+  socket.on("deleteChat", (chatname) => {
+    const socketsInRoom = io.sockets.adapter.rooms.get(chatname);
+    if (socketsInRoom) {
+      socket.to(chatname).emit("roomDeleted", `Room ${chatname} has been deleted.`);
+      for (const socketId of socketsInRoom) {
+        const socketToDisconnect = io.sockets.sockets.get(socketId);
+        socketToDisconnect.leave(chatname);
+      }
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+
+const PORT = process.env.PORT || 4000;
+httpServer.listen(PORT, () => {
+  console.log(`Socket.io server running on port ${PORT}`);
+});
